78bd285cb398f3296936dc517b139192acf7ea6a
NSHIMIYIMANA <joanesclaudio10@gmail.com>
2025-01-09 21:13:24 -0800

    added quiz app

---
+++ Documents/MY QUIZ APP/my-quiz/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import QuizData from './components/QuizData';
+
+function App() {
+
+  return (
+    <>
+      <QuizData />
+    </>
+  )
+}
+
+export default App
